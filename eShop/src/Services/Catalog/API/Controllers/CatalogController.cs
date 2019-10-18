06c76ccdfedef48d447a057fb3adfdcaa3e61004
Kim Jørgensen <kim.joergensen@outlook.com>
2019-10-18 22:08:15 +0200

    catalog API initialize

---
+++ eShop/src/Services/Catalog/API/Controllers/CatalogController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        [HttpGet]
+        [MapToApiVersion("1")]
+        public ActionResult Get() {
+            return Ok();
+        }
+    }
+}
---
+++ eShop/src/Services/Catalog/API/Infrastructure/Filters/ReplaceVersionWithExactValueInPath.cs
@@ -0,0 +1,19 @@
+﻿using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Linq;
+
+namespace API.Infrastructure.Filters
+{
+    /// <summary>
+    /// Replace the end-point path from being variable as "api/v{version}/[controller]
+    /// to having a fixed path e.g. "api/v1/[controller].
+    /// </summary>
+    public class ReplaceVersionWithExactValueInPath : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, DocumentFilterContext context) {
+            swaggerDoc.Paths = swaggerDoc.Paths
+                .ToDictionary(path => path.Key.Replace("v{version}", swaggerDoc.Info.Version),
+                              path => path.Value);
+        }
+    }
+}
